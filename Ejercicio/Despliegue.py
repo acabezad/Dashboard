0a26a0ae42946b36da79271ba3284706b8aec37f
Andrés Camilo Cabeza Díaz <69399661+acabezad@users.noreply.github.com>
2020-12-05 17:20:54 -0500

    Update Despliegue.py

--- Ejercicio/Despliegue.py
+++ Ejercicio/Despliegue.py
@@ -22,7 +22,7 @@ from dash.dependencies import Input, Output
 import dash
 import plotly.graph_objs as go
 import plotly.express as px
-from jupyter_dash import JupyterDash
+
 
 
 #Carga base de datos
